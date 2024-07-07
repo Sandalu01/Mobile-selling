de606999c694c9974aa84efc20db607affba5f8c
sandaluthushan20 <sandaluthushan20@gmail.com>
2024-07-07 08:45:55 +0530

    📱

--- mobile-sell/src/Components/LoginPage.jsx
+++ mobile-sell/src/Components/LoginPage.jsx
@@ -11,6 +11,7 @@ const LoginPage = () => {
         <div className='login-fo'>
         <h2 className="login-title">Login</h2>
       <Form className="login-form">
+        
         <Form.Group controlId="formBasicEmail">
           <Form.Label className="login-label">Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" className="login-input" />
@@ -22,7 +23,9 @@ const LoginPage = () => {
         <Button variant="primary" type="submit" className="login-button">
           Submit
         </Button>
+  
       </Form>
+      
         </div>
     </Container>
 
--- mobile-sell/src/Components/SignupForm.jsx
+++ mobile-sell/src/Components/SignupForm.jsx
@@ -17,9 +17,9 @@ const SignupPage = () => {
 
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="E
-          nter email" />
+          <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
+       
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
