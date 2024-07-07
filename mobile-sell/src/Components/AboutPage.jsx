5966e75b84772712eca730e48cabc9af5fa423bd
sandaluthushan20 <sandaluthushan20@gmail.com>
2024-07-07 08:30:34 +0530

    changes icon

--- mobile-sell/src/Components/AboutPage.jsx
+++ mobile-sell/src/Components/AboutPage.jsx
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const AboutUs = () => {
   return (
+    <div className="about">
     <Container className="about-us">
       <Row className="justify-content-md-center">
         <Col md="8">
@@ -33,6 +34,7 @@ const AboutUs = () => {
         </Col>
       </Row>
     </Container>
+    </div>
   );
 };
 
