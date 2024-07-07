364e5dbc4d3bb22136ec9cdaa45db347d632adc2
sandaluthushan20 <sandaluthushan20@gmail.com>
2024-07-07 08:56:14 +0530

    added nav icon

--- mobile-sell/src/Components/NaviBar.jsx
+++ mobile-sell/src/Components/NaviBar.jsx
@@ -2,19 +2,20 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { FaHome, FaSignInAlt, FaUserPlus, FaInfoCircle } from 'react-icons/fa';
 
 const NaviBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/">Mobile Store</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/"> 📱 MobileMart</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/login">Login</Nav.Link>
-            <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
-            <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/"><FaHome /> Home</Nav.Link>
+            <Nav.Link as={Link} to="/login"> <FaSignInAlt /> Login</Nav.Link>
+            <Nav.Link as={Link} to="/signup"> <FaUserPlus /> Sign Up</Nav.Link>
+            <Nav.Link as={Link} to="/about"> <FaInfoCircle /> About Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
