d12c8d8f94010ec0b5246f9386212b6ddb979dc2
sandaluthushan20 <sandaluthushan20@gmail.com>
2024-07-13 08:10:56 +0530

    aded accoriding

--- mobile-sell/src/Components/HomePage.jsx
+++ mobile-sell/src/Components/HomePage.jsx
@@ -1,13 +1,12 @@
 // src/components/HomePage.js
 import React from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
-
-
-
+import Accordion from 'react-bootstrap/Accordion';
 
 
 const HomePage = () => {
   return (
+  <>
    
     <Container className="mt-5">
       <Row className="mb-4">
@@ -55,6 +54,33 @@ const HomePage = () => {
         </Col>
       </Row>
     </Container>
+     <Accordion defaultActiveKey="0">
+     <Accordion.Item eventKey="0">
+       <Accordion.Header>Accordion Item #1</Accordion.Header>
+       <Accordion.Body>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+         minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+         aliquip ex ea commodo consequat. Duis aute irure dolor in
+         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+         culpa qui officia deserunt mollit anim id est laborum.
+       </Accordion.Body>
+     </Accordion.Item>
+     <Accordion.Item eventKey="1">
+       <Accordion.Header>Accordion Item #2</Accordion.Header>
+       <Accordion.Body>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+         minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+         aliquip ex ea commodo consequat. Duis aute irure dolor in
+         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+         culpa qui officia deserunt mollit anim id est laborum.
+       </Accordion.Body>
+     </Accordion.Item>
+   </Accordion>
+   </>
 
   );
 };
