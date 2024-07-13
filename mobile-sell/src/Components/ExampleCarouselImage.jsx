7d3126ea3a1c522da8ed16e13124663ff087bb56
sandaluthushan20 <sandaluthushan20@gmail.com>
2024-07-14 01:45:56 +0530

    size changes

--- mobile-sell/src/Components/ExampleCarouselImage.jsx
+++ mobile-sell/src/Components/ExampleCarouselImage.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ExampleCarouselImage = () => {
     return (
-        <div>
-        
-            <img src="" alt="Example Image" />
-            <p>This is an example image for the carousel.</p>
-        </div>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    style={{ width: '300px', height: '200px' }} 
+                    className="d-block w-100"
+                    src="https://images.unsplash.com/photo-1589492477829-5e65395b66cc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG1vYmlsZSUyMHBob25lfGVufDB8fDB8fHww"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                 style={{ width: '100px', height: '200px' }}
+                    className="d-block w-100"
+                    src="https://via.placeholder.com/800x400?text=Second+Slide"
+                    alt="Second slide"
+                />
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://via.placeholder.com/800x400?text=Third+Slide"
+                    alt="Third slide"
+                />
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
