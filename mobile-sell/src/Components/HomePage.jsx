0c79ba5c4b3fe31a3823fe4b30427c9af46fbb65
sandaluthushan20 <sandaluthushan20@gmail.com>
2024-07-14 00:40:10 +0530

    added iamges

--- mobile-sell/src/Components/HomePage.jsx
+++ mobile-sell/src/Components/HomePage.jsx
@@ -1,10 +1,19 @@
 // src/components/HomePage.js
 import React from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button ,ExampleCarouselImage} from 'react-bootstrap';
 import Accordion from 'react-bootstrap/Accordion';
 
 
+
+
 const HomePage = () => {
+
+  const [index, setIndex] = useState(0);
+  
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
   return (
   <>
    
@@ -80,8 +89,33 @@ const HomePage = () => {
        </Accordion.Body>
      </Accordion.Item>
    </Accordion>
-
+   <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <ExampleCarouselImage text="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExampleCarouselImage text="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExampleCarouselImage text="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
    </Container>
+
    </>
 
   );
