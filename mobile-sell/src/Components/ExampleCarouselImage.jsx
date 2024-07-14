c8e18b750e77b7d27bc1db71e17843043bf4d1ab
sandaluthushan20 <sandaluthushan20@gmail.com>
2024-07-14 20:18:22 +0530

    ss

--- mobile-sell/src/Components/ExampleCarouselImage.jsx
+++ mobile-sell/src/Components/ExampleCarouselImage.jsx
@@ -7,38 +7,26 @@ const ExampleCarouselImage = () => {
         <Carousel>
             <Carousel.Item>
                 <img
-                    style={{ width: '300px', height: '200px' }} 
-                    className="d-block w-100"
+                     style={{ width: '400px', height: '400px' }}
                     src="https://images.unsplash.com/photo-1589492477829-5e65395b66cc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG1vYmlsZSUyMHBob25lfGVufDB8fDB8fHww"
                     alt="First slide"
                 />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                 style={{ width: '100px', height: '200px' }}
-                    className="d-block w-100"
-                    src="https://via.placeholder.com/800x400?text=Second+Slide"
+                    style={{ width: '400px', height: '400px' }}
+                    src="https://images.unsplash.com/photo-1523206489230-c012c64b2b48?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGhvbmV8ZW58MHx8MHx8fDA%3D"
                     alt="Second slide"
                 />
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
+
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
+                    style={{ width: '400px', height: '400px' }}
                     src="https://via.placeholder.com/800x400?text=Third+Slide"
                     alt="Third slide"
                 />
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
+
             </Carousel.Item>
         </Carousel>
     );
