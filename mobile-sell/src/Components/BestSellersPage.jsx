64b89881007d64857c1c95c24f970cc05f925c49
sandaluthushan20 <sandaluthushan20@gmail.com>
2024-07-15 08:34:52 +0530

    new

--- mobile-sell/src/Components/BestSellersPage.jsx
+++ mobile-sell/src/Components/BestSellersPage.jsx
@@ -3,7 +3,7 @@ import { Container, Row, Col, Card } from 'react-bootstrap';
 
 const BestSellersPage = () => {
   return (
-    <Container className="mt-5">
+    <Container className="best">
       <Row className="mb-4">
         <Col>
           <h1>Best Sellers</h1>
