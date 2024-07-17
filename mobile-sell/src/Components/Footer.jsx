0a3609d923546f787b9bf61cb0b68b9bab09a2d1
sandaluthushan20 <sandaluthushan20@gmail.com>
2024-07-17 08:44:19 +0530

    footer changes

--- mobile-sell/src/Components/Footer.jsx
+++ mobile-sell/src/Components/Footer.jsx
@@ -1,6 +1,6 @@
-// src/components/Footer.js
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -9,6 +9,28 @@ const Footer = () => {
         <Row>
           <Col>
             <p>&copy; {new Date().getFullYear()} Mobile Store. All Rights Reserved.</p>
+          </Col>
+        </Row>
+        <Row className="mt-3">
+          <Col>
+            <a href="https://www.facebook.com" className="text-white mx-2" aria-label="Facebook">
+              <FaFacebook size={30} />
+            </a>
+            <a href="https://www.twitter.com" className="text-white mx-2" aria-label="Twitter">
+              <FaTwitter size={30} />
+            </a>
+            <a href="https://www.instagram.com" className="text-white mx-2" aria-label="Instagram">
+              <FaInstagram size={30} />
+            </a>
+            <a href="https://www.linkedin.com" className="text-white mx-2" aria-label="LinkedIn">
+              <FaLinkedin size={30} />
+            </a>
+          </Col>
+        </Row>
+        <Row className="mt-3">
+          <Col>
+            <p>123 Main Street, Colombo, Sri Lanka</p>
+            <p>Email: info@mymobilestore.com | Phone: +94 123 456 789</p>
           </Col>
         </Row>
       </Container>
